4ee2bcb91fc17f6581e6b0085c8659f0bc9f0440
Bouchra KOUACH <bouchra.kouach@sii-maroc.com>
2023-06-06 11:56:25 +0100

    test

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main ( String[] args) {
-        System.out.println("hello, KOUACH et bienvenue chez java <3");
+        System.out.println("hello, KOUACH et bienvenue chez java <3  , et merci ");
         }
 }
